974025e0f0b2a82ea463e0f5a3ce3c9b85e93118
Duy Chung <duy.chung@formos.com>
2018-04-03 10:24:46 +0700

    get comments dynamically

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -3,15 +3,15 @@ import Comment from './Comment';
 
 class CommentBox extends React.Component {
 
-    // getComments() {
-    //     const commentList = [
-    //         {id: 1, author: 'Duy Chung 1', body: 'Hello 1'},
-    //         {id: 2, author: 'Duy Chung 2', body: 'Hello 2'}
-    //     ];
-    //     return commentList.map((item) => {
-    //         return ( <Comment author={item.author} body={item.body} key={item.id} /> );
-    //     });
-    // }
+    getComments() {
+        const commentList = [
+            {id: 1, author: 'Duy Chung 1', body: 'Hello 1'},
+            {id: 2, author: 'Duy Chung 2', body: 'Hello 2'}
+        ];
+        return commentList.map((item) => {
+            return ( <Comment author={item.author} body={item.body} key={item.id} /> );
+        });
+    }
 
     render() {
         return (
@@ -19,8 +19,7 @@ class CommentBox extends React.Component {
                 <h3>Comments</h3>
                 <h4 className="comment-count">2 comments</h4>
                 <div className="comment-list">
-                    <Comment author="Duy Chung 1" body="Hello 1" />
-                    <Comment author="Duy Chung 2" body="Hello 2" />
+                    {this.getComments()}
                 </div>
             </div>
             
